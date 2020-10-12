4320248a7dbd271747d7865c2df680667e444ccb
ue8d <webmaster@tomoki12.com>
2020-10-12 16:32:48 +0900

    ボタンを押して写真撮影を可能に

--- src/js/camera.js
+++ src/js/camera.js
@@ -12,3 +12,16 @@ const $video = document.getElementById('video_area');  // 映像表示エリア
 navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(stream => $video.srcObject = stream)
     .catch(err => alert(`${err.name} ${err.message}`));
+
+// 「静止画取得」ボタンが押されたら「<canvas id="capture_image">」に映像のコマ画像を表示します。
+const btn = document.getElementById('shutter_btn');
+
+btn.addEventListener('click', function() {
+    const canvas_capture_image = document.getElementById('capture_image');
+    const cci = canvas_capture_image.getContext('2d');
+    const va = document.getElementById('video_area');
+
+    canvas_capture_image.width  = va.videoWidth;
+    canvas_capture_image.height = va.videoHeight;
+    cci.drawImage(va, 0, 0);  // canvasに『「静止画取得」ボタン』押下時点の画像を描画。
+})
