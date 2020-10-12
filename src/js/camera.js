b04491b0ed0348ded203d698e1995853de1999b3
ue8d <webmaster@tomoki12.com>
2020-10-12 23:11:11 +0900

    Apacheサーバーでは動く形に

--- src/js/camera.js
+++ src/js/camera.js
@@ -6,22 +6,28 @@ if (typeof navigator.mediaDevices.getUserMedia !== 'function') {
 }
 
 // 操作する画面エレメント変数定義
+const $start = document.getElementById('start_btn');
 const $video = document.getElementById('video_area');  // 映像表示エリア
 
 // カメラ許可を得たら、getUserMedia を使って映像を「映像表示エリア」に表示
-navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(stream => $video.srcObject = stream)
-    .catch(err => alert(`${err.name} ${err.message}`));
+$start.addEventListener('click', () => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(stream => $video.srcObject = stream)
+        .catch(err => alert(`${err.name} ${err.message}`));
+}, false);
+
 
 // 「静止画取得」ボタンが押されたら「<canvas id="capture_image">」に映像のコマ画像を表示します。
 const btn = document.getElementById('shutter_btn');
 
-btn.addEventListener('click', function() {
-    const canvas_capture_image = document.getElementById('capture_image');
-    const cci = canvas_capture_image.getContext('2d');
-    const va = document.getElementById('video_area');
+function copyFrame() {
+    var canvas_capture_image = document.getElementById('capture_image');
+    var cci = canvas_capture_image.getContext('2d');
+    var va = document.getElementById('video_area');
 
-    canvas_capture_image.width  = va.videoWidth;
+    canvas_capture_image.width = va.videoWidth;
     canvas_capture_image.height = va.videoHeight;
-    cci.drawImage(va, 0, 0);  // canvasに『「静止画取得」ボタン』押下時点の画像を描画。
-})
+    cci.drawImage(va, 0, 0);
+
+    processImage();
+}
---
+++ src/js/face.js
@@ -0,0 +1,59 @@
+function processImage() {
+    // Replace <Subscription Key> with your valid subscription key.
+    var subscriptionKey = "Subscription Key";
+
+    var uriBase =
+        "https://<My Endpoint String>.com/face/v1.0/detect";
+
+    // Request parameters.
+    var params = {
+        "returnFaceId": "true",
+        "returnFaceLandmarks": "false",
+        "returnFaceAttributes":
+            "age,gender,headPose,smile,facialHair,glasses,emotion," +
+            "hair,makeup,occlusion,accessories,blur,exposure,noise"
+    };
+
+    // Display the image.
+    var type = 'image/jpeg';
+    var dataurl = capture_image.toDataURL(type);
+    var bin = atob(dataurl.split(',')[1]);
+    var buffer = new Uint8Array(bin.length);
+    for (var i = 0; i < bin.length; i++) {
+        buffer[i] = bin.charCodeAt(i);
+    }
+    var blob = new Blob([buffer.buffer], { type: type });
+
+    // Perform the REST API call.
+    $.ajax({
+        url: uriBase + "?" + $.param(params),
+
+        // Request headers.
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+
+        processData: false,
+        type: "POST",
+
+        // Request body.
+        data: blob,
+    })
+
+        .done(function (data) {
+            // Show formatted JSON on webpage.
+            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        })
+
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ?
+                "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ?
+                "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+                    jQuery.parseJSON(jqXHR.responseText).message :
+                    jQuery.parseJSON(jqXHR.responseText).error.message;
+            alert(errorString);
+        });
+};
